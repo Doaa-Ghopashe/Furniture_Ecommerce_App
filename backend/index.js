f35fd144bcdfa2e22bb10163613e2f088aa81862
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-09-08 20:48:09 +0300

    build an image and its container

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ PORT = process.env.PORT || 4000,
 
 app = express();
 
-app.get('/',(req,res)=>{res.send("<h1>Hello World</h1>")});
+app.get('/',(req,res)=>{res.send("<h1>Hello World Doaa</h1>")});
 
 app.listen(PORT,()=>{console.log("listening on port")});
 
