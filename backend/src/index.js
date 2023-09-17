73849668b0a2fff37f4f9cb6112b4cae47b87954
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-09-17 10:56:46 +0300

    practicing on docker

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ PORT = process.env.PORT || 4000,
 
 app = express();
 
-app.get('/',(req,res)=>{res.send("<h1>Doaa Ghopashe</h1>")});
+app.get('/',(req,res)=>{res.send("<h1>Doaa Adel Ghopashe mohamed de</h1>")});
 
 app.listen(PORT,()=>{console.log("listening on port")});
 
